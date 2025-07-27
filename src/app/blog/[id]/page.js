e6a00b8004a05dae4ee708047f3104efd1c75b32
Cemregnr <cemrgnr9@gmail.com>
2025-07-27 13:02:08 +0300

    contact

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -16,23 +16,32 @@ export default async function Slug({ params }) {
   return (
     <div className="flex container mx-auto">
       {data.image_url && (
-        <Image
-          src={data.image_url}
-          width={300}
-          height={200}
-          className="object-cover flex container object-left  w-dvh rounded-bl-full  rounded-br-full pr-20 pl-5 pt-30 pb-30"
-          alt=""
-        />
+        <div className="border-4 border-yellow-200 m-5 rounded-lg mb-auto mr-30 object-contain">
+          <Image
+            src={data.image_url}
+            width={300}
+            height={200}
+            className="object-cover flex container object-left  w-dvh rounded-bl-full  rounded-br-full pr-20 pl-5 pt-30 pb-30"
+            alt=""
+          />{" "}
+        </div>
       )}
       <div className="flex container mx-auto  flex-col items-center pt-6">
         <div>
-          <h1 className="text-amber-900   text-3xl  border-t-amber-300  font-extrabold font-serif  text-center">
-            {data.title}
-          </h1>{" "}
+          <div className="border-4 border-yellow-200 p-5 rounded-lg mb-4 w-full max-w-3xl">
+            <h1 className="text-amber-900   text-3xl  border-t-amber-300  font-extrabold font-serif  text-center">
+              {data.title}
+            </h1>{" "}
+          </div>{" "}
         </div>
-        <div className=" flex container mx-auto text-center pl-20   mt-5  font-serif font-bold text-gray-200"></div>{" "}
-        {data.content}
-        {data.content}
+        <div className="border-4 border-yellow-200 p-5 rounded-lg mb-4 w-full max-w-3xl">
+          <div className=" flex container mx-auto text-center  pl-20  ml-20 mt-5  font-serif font-bold  mb-auto text-gray-200">
+            {" "}
+          </div>
+
+          {data.content}
+          {data.content}
+        </div>{" "}
       </div>
     </div>
   );
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,3 +1,23 @@
-export default function Contact() {
-  return <h1>Contact</h1>;
+import Image from "next/image";
+
+export default function contact() {
+  return (
+    <>
+      <div className="p-5 rounded-lg mb-4 w-full max-w-3xl mx-auto flex container">
+        <h1 className="text-amber-900   text-3xl  border-t-amber-300  font-extrabold font-serif  mx-auto border-b-4  ">
+          Contact Us
+        </h1>
+      </div>
+      <div className="flex container mx-auto">
+        <Image
+          src="https://www.usmarshals.gov/sites/default/files/styles/hero/public/acquiadam/contact-us-2400x1320.webp"
+          alt=""
+          width={300}
+          height={500}
+          fill
+          className="object-cover flex container  rounded-bl-full  rounded-br-full "
+        />
+      </div>
+    </>
+  );
 }
